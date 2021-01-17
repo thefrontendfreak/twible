f18e9ca11e8c377240a0fdfc84d301fac4623bc1
Sunny Ahmed <thefrontendfreak@gmail.com>
2021-01-17 17:56:59 +0600

    new update

--- js/app.js
+++ js/app.js
@@ -2,4 +2,30 @@ $(function () {
     $("#accordion").accordion({
         heightStyle: "content"
     });
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dot:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
 });
+
+var toggleButton = document.getElementById("toggle");
+var toggleMenu = document.getElementById("menu");
+
+document.getElementById("toggle").addEventListener("click", function() {
+    toggleButton.classList.toggle('active');
+    toggleMenu.classList.toggle('showMenu');
+});
+
